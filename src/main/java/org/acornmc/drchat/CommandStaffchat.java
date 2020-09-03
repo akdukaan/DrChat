b9042f8b11c3679586253c5854467cf118d68356
DrBot7 <46543473+DrBot7@users.noreply.github.com>
2020-09-02 19:39:29 -0500

    shorthand hex support

--- src/main/java/org/acornmc/drchat/CommandStaffchat.java
+++ src/main/java/org/acornmc/drchat/CommandStaffchat.java
@@ -9,8 +9,13 @@ import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 import org.jetbrains.annotations.NotNull;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class CommandStaffchat implements CommandExecutor {
-        ConfigManager configManager;
+    ConfigManager configManager;
+    private static final Pattern HEX_PATTERN = Pattern.compile("&#([0-9A-F]{6})", Pattern.CASE_INSENSITIVE);
+
 
     public CommandStaffchat(ConfigManager configManager) {
         this.configManager = configManager;
@@ -36,7 +41,7 @@ public class CommandStaffchat implements CommandExecutor {
         String message = String.join(" ", args);
         String mcToMc = configManager.get().getString("messages.staffchat.mc-to-mc-format");
         mcToMc = addPlaceholders(mcToMc, player, message);
-        mcToMc = ChatColor.translateAlternateColorCodes('&', mcToMc);
+        mcToMc = convertHex(mcToMc);
         Bukkit.broadcast(mcToMc, "drchat.staffchat");
         if (Bukkit.getPluginManager().isPluginEnabled("DiscordSRV")) {
             Bukkit.getServer().getScheduler().runTaskAsynchronously(configManager.plugin, () ->
@@ -50,5 +55,21 @@ public class CommandStaffchat implements CommandExecutor {
         format = format.replace("%nickname%", player.getDisplayName());
         format = format.replace("%message%", message);
         return format;
+    }
+
+    public static String convertHex(String in) {
+        Matcher matcher = HEX_PATTERN.matcher(in);
+        while (matcher.find()) {
+            in = matcher.replaceFirst("&x" + addBeforeAllChars(matcher.group(1)));
+        }
+        return in;
+    }
+
+    private static String addBeforeAllChars(String string) {
+        StringBuilder builder = new StringBuilder(string.length() * 2);
+        for (char existing : string.toCharArray()) {
+            builder.append('&').append(existing);
+        }
+        return builder.toString();
     }
 }
