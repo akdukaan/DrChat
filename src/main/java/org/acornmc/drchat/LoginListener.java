5e1ead2c5effaa293823d1beaa94cafb9bec6aad
Kaan <drbot7@gmail.com>
2021-12-06 14:49:47 -0600

    simplify and remove bloat

--- src/main/java/org/acornmc/drchat/LoginListener.java
+++ src/main/java/org/acornmc/drchat/LoginListener.java
@@ -2,66 +2,22 @@ package org.acornmc.drchat;
 
 import com.earth2me.essentials.Essentials;
 import com.earth2me.essentials.User;
-import org.bukkit.Bukkit;
-import org.bukkit.ChatColor;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerLoginEvent;
-import org.bukkit.plugin.Plugin;
-import org.bukkit.scheduler.BukkitRunnable;
 
 public class LoginListener implements Listener {
 
     @EventHandler
     public void onLogin(PlayerLoginEvent event) {
         Player player = event.getPlayer();
-        if (shouldRemovePrefix(player)) {
-            removePrefix(player);
-        }
-        if (shouldRemoveNickname(player)) {
-            removeNickname(player);
-        }
-    }
-
-    public boolean shouldRemovePrefix(Player player) {
-        if (!player.hasPermission("drchat.removeprefixonlogin")) return false;
-        return !player.hasPermission("drchat.removeprefixonloginexempt");
-    }
-
-    public boolean shouldRemoveNickname(Player player) {
-        if (!player.hasPermission("drchat.removenickonlogin")) return false;
-        if (hasColoredNick(player) && !player.hasPermission("drchat.removenickonloginexempt")) return true;
-        Essentials ess = EssentialsHook.getEssentials();
-        if (ess == null) return false;
-        User user = ess.getUser(player.getUniqueId());
-        if (user == null) return false;
-        return !user.getLastAccountName().equals(user.getName());
-    }
-
-    public boolean hasColoredNick(Player player) {
-        Essentials ess = EssentialsHook.getEssentials();
-        if (ess == null) return false;
-        User user = ess.getUser(player.getUniqueId());
-        if (user == null) return false;
-        String nick = user.getNickname();
-        if (nick == null) return false;
-        return nick.contains(ChatColor.COLOR_CHAR + "");
-    }
-
-    public void removePrefix(Player player) {
-        Plugin plugin = DrChat.getInstance();
-        new BukkitRunnable() {
-            @Override
-            public void run() {
-                Bukkit.dispatchCommand(Bukkit.getConsoleSender(), "lp user " + player.getName() + " meta removeprefix 1");
-            }
-        }.runTask(plugin);
-    }
-
-    public void removeNickname(Player player) {
+        if (!player.hasPermission("drchat.resetnickonnamechange")) return;
         Essentials ess = EssentialsHook.getEssentials();
         if (ess == null) return;
-        ess.getUser(player.getUniqueId()).setNickname(player.getName());
+        User user = ess.getUser(player.getUniqueId());
+        if (user == null) return;
+        if (user.getLastAccountName().equals(user.getName())) return;
+        user.setNickname(player.getName());
     }
 }
